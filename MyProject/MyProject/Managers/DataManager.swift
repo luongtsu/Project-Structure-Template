d15854b8fb6eaacd28022102d5e3c7dee7f2e229
Hiep Luong Minh <hluongminh@pentalog.fr>
2017-10-16 06:02:23 +0700

    Arrange folders

---
+++ MyProject/MyProject/Managers/DataManager.swift
@@ -0,0 +1,9 @@
+//
+//  DataManager.swift
+//  MyProject
+//
+//  Created by Luong Minh Hiep on 10/16/17.
+//  Copyright © 2017 Luong Minh Hiep. All rights reserved.
+//
+
+import Foundation
---
+++ MyProject/MyProject/Models/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  MyProject
+//
+//  Created by Luong Minh Hiep on 10/16/17.
+//  Copyright © 2017 Luong Minh Hiep. All rights reserved.
+//
+
+import Foundation
